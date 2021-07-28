08ac0aaeb0cce9458ce8ad3fa68b8c0873cb6444
Noor  Yasser <mrh61037@gmail.com>
2021-07-28 18:38:29 +0300

    add HomeFragment.kt

---
+++ app/src/main/java/com/noor/yasser/ps/githubapp/ui/fragments/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.noor.yasser.ps.githubapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.noor.yasser.ps.githubapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private val mBinding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = mBinding.root
+
+
+}
