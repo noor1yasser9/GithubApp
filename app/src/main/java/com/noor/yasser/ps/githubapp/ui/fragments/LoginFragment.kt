ee2b2c3fb3fb5420e16a7deb4e81bfe8528bed5f
Noor  Yasser <mrh61037@gmail.com>
2021-07-28 18:38:44 +0300

    update Login Fragment

--- app/src/main/java/com/noor/yasser/ps/githubapp/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/noor/yasser/ps/githubapp/ui/fragments/LoginFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.noor.yasser.ps.githubapp.R
 import com.noor.yasser.ps.githubapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -19,5 +21,13 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ) = mBinding.root
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding.mBtnSignIn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+    }
 
 }
