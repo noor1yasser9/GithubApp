eccb69f98c37b88bb253bf40fa44c6164c5ff040
Noor  Yasser <mrh61037@gmail.com>
2021-07-28 18:39:08 +0300

    add file utils Class Gson Utils TimeConverter.kt

---
+++ app/src/main/java/com/noor/yasser/ps/githubapp/utils/GsonUtils.kt
@@ -0,0 +1,16 @@
+package com.noor.yasser.ps.githubapp.utils
+
+import com.google.gson.Gson
+
+object GsonUtils {
+
+    val INSTANCE: Gson = Gson()
+}
+
+fun <T> T.toJson(): String {
+    return GsonUtils.INSTANCE.toJson(this)
+}
+
+inline fun <reified T> String.fromJson(): T {
+    return GsonUtils.INSTANCE.fromJson(this, T::class.java)
+}
