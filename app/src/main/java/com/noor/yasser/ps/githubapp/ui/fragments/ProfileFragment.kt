1604c692034f5b4396ba13df99a88ba9acbfe286
Noor  Yasser <mrh61037@gmail.com>
2021-08-01 18:32:03 +0300

    add Fragment Profile

---
+++ app/src/main/java/com/noor/yasser/ps/githubapp/ui/fragments/ProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.noor.yasser.ps.githubapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.noor.yasser.ps.githubapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
