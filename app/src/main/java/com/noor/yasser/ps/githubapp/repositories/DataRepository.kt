99458bc3b6b46b905a9b6599e05d8d9875a38aca
Noor  Yasser <mrh61037@gmail.com>
2021-08-07 18:02:49 +0300

    add repositories DataRepository.kt

---
+++ app/src/main/java/com/noor/yasser/ps/githubapp/repositories/DataRepository.kt
@@ -0,0 +1,11 @@
+package com.noor.yasser.ps.githubapp.repositories
+
+import com.noor.yasser.ps.githubapp.network.DataInterface
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DataRepository @Inject constructor(val dataInterface: DataInterface) {
+
+
+}
