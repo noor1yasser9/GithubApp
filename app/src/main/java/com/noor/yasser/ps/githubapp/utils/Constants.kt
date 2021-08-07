558a7e2adbed4c1e111377b9908def67405eb76b
Noor  Yasser <mrh61037@gmail.com>
2021-08-07 18:03:10 +0300

    add Constants.kt Add ViewModels

---
+++ app/src/main/java/com/noor/yasser/ps/githubapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.noor.yasser.ps.githubapp.utils
+
+
+const val BASE_URL ="https://api.github.com/"
+const val KET_AUTH = "Authorization"
+const val VALUE_AUTH = "mau API KEY? Tonton video tutorialnya ya^^"
---
+++ app/src/main/java/com/noor/yasser/ps/githubapp/viewmodels/HomeViewModel.kt
@@ -0,0 +1,14 @@
+package com.noor.yasser.ps.githubapp.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.noor.yasser.ps.githubapp.repositories.DataRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    val dataRepository: DataRepository,
+    application: Application
+) : AndroidViewModel(application) {
+}
