33cbc9047e08f5039b7cf8c7417ab6a0caa53da6
Noor  Yasser <mrh61037@gmail.com>
2021-08-08 14:17:05 +0300

    update instance retrofit

--- app/src/main/java/com/noor/yasser/ps/githubapp/di/RetrofitModule.kt
+++ app/src/main/java/com/noor/yasser/ps/githubapp/di/RetrofitModule.kt
@@ -1,13 +1,17 @@
 package com.noor.yasser.ps.githubapp.di
 
 
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.noor.yasser.ps.githubapp.network.DataInterface
 import com.noor.yasser.ps.githubapp.utils.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -17,30 +21,66 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object RetrofitModule {
 
+
+
+
+
+
+
     @Provides
     @Singleton
-    fun InstanceRetrofit() =
+    fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = when (DEBUG) {
+                true -> HttpLoggingInterceptor.Level.BODY
+                false -> HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun providesChucker(@ApplicationContext context: Context): ChuckerInterceptor {
+        return ChuckerInterceptor(context)
+    }
+
+    @Provides
+    @Singleton
+    fun providesApiKey(): Interceptor = Interceptor { chain ->
+        chain.proceed( chain.request().newBuilder()
+            .header(KET_AUTH, VALUE_AUTH)
+            .build())
+    }
+
+    @Provides
+    @Singleton
+    fun providesHttpClient(
+        interceptor: HttpLoggingInterceptor,
+        chucker: ChuckerInterceptor,
+        apiKey: Interceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            retryOnConnectionFailure(true)
+            readTimeout(30, TimeUnit.SECONDS)
+            writeTimeout(30, TimeUnit.SECONDS)
+            addInterceptor(interceptor)
+            if (DEBUG) addInterceptor(chucker)
+            addInterceptor(apiKey)
+        }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun InstanceRetrofit(client: OkHttpClient) =
         Retrofit.Builder()
+            .client(client)
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .apply {
-                val builder = OkHttpClient.Builder()
-                builder.authenticator { route, response ->
-                    response.request.newBuilder()
-                        .header(KET_AUTH, VALUE_AUTH)
-                        .build();
-                }
-                builder.connectTimeout(5, TimeUnit.MINUTES) // connect timeout
-                    .writeTimeout(5, TimeUnit.MINUTES) // write timeout
-                    .readTimeout(5, TimeUnit.MINUTES) // read timeout
-
-                this.client(builder.build())
-            }
             .build()
 
 
     @Provides
     @Singleton
-    fun dataInterface() =
-        InstanceRetrofit().create(DataInterface::class.java)
+    fun dataInterface(retrofit: Retrofit) =
+        retrofit.create(DataInterface::class.java)
 }
