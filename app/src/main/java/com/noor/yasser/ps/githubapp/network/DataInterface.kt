0127e627f4fbe2c2f954db98d17e689e5c59a557
Noor  Yasser <mrh61037@gmail.com>
2021-08-08 14:17:52 +0300

    add function get User Details

--- app/src/main/java/com/noor/yasser/ps/githubapp/network/DataInterface.kt
+++ app/src/main/java/com/noor/yasser/ps/githubapp/network/DataInterface.kt
@@ -1,4 +1,13 @@
 package com.noor.yasser.ps.githubapp.network
 
+import com.noor.yasser.ps.githubapp.model.UserModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface DataInterface {
+
+    @GET("/users/{username}")
+    suspend fun detailUser(@Path("username") username: String): Response<UserModel>
+
 }
