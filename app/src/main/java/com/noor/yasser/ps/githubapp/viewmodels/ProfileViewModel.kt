3a84201484f0886191513cf111534bfcfbbf6a50
Noor  Yasser <mrh61037@gmail.com>
2021-08-08 14:20:45 +0300

    update view model
    update resorce
    
    add library

--- app/src/main/java/com/noor/yasser/ps/githubapp/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/com/noor/yasser/ps/githubapp/viewmodels/ProfileViewModel.kt
@@ -2,8 +2,10 @@ package com.noor.yasser.ps.githubapp.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.noor.yasser.ps.githubapp.repositories.DataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,4 +13,15 @@ class ProfileViewModel @Inject constructor(
     val dataRepository: DataRepository,
     application: Application
 ) : AndroidViewModel(application) {
+
+
+    fun detailUser(username: String, isExists: (Boolean) -> Unit) {
+        viewModelScope.launch {
+            dataRepository.detailUser(username,isExists)
+        }
+    }
+
+    fun getUserDataStateFlow() =
+        dataRepository.getUserDataStateFlow()
+
 }
