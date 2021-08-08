598aff08e559329f4b480e9a134341eb8153a724
Noor  Yasser <mrh61037@gmail.com>
2021-08-08 14:18:17 +0300

    get Data User Details to StateFlow

--- app/src/main/java/com/noor/yasser/ps/githubapp/repositories/DataRepository.kt
+++ app/src/main/java/com/noor/yasser/ps/githubapp/repositories/DataRepository.kt
@@ -1,11 +1,56 @@
 package com.noor.yasser.ps.githubapp.repositories
 
 import com.noor.yasser.ps.githubapp.network.DataInterface
+import com.noor.yasser.ps.githubapp.utils.ResultResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DataRepository @Inject constructor(val dataInterface: DataInterface) {
 
+    private val userDataMutableStateFlow: MutableStateFlow<ResultResponse<Any>> =
+        MutableStateFlow(ResultResponse.loading(""))
 
+
+    fun detailUser(username: String, isExists: (Boolean) -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = dataInterface.detailUser(username)
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        if (it.message == null) {
+                            userDataMutableStateFlow.emit(ResultResponse.success(it))
+                            isExists(true)
+                        } else {
+                            isExists(false)
+                        }
+                    }
+                } else {
+                    userDataMutableStateFlow.emit(
+                        ResultResponse.error(
+                            "Ooops: ${response.errorBody()}",
+                            response
+                        )
+                    )
+                    isExists(false)
+                }
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                userDataMutableStateFlow.emit(ResultResponse.error("Ooops: ${e.message()}", e))
+                isExists(false)
+            } catch (t: Throwable) {
+                t.printStackTrace()
+                userDataMutableStateFlow.emit(ResultResponse.error("Ooops: ${t.message}", t))
+                isExists(false)
+            }
+        }
+    }
+
+    fun getUserDataStateFlow(): StateFlow<ResultResponse<Any>> = userDataMutableStateFlow
 }
