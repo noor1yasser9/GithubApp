42b3cff1f65613099a62f302ab99478e9cb99661
Noor  Yasser <mrh61037@gmail.com>
2021-08-09 19:16:47 +0300

    create instance with daggar for room database

---
+++ app/src/main/java/com/noor/yasser/ps/githubapp/di/DataBaseModule.kt
@@ -0,0 +1,31 @@
+package com.noor.yasser.ps.githubapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.noor.yasser.ps.githubapp.db.GithubDAO
+import com.noor.yasser.ps.githubapp.db.GithubDB
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataBaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDB(application: Application): GithubDB {
+        return Room
+            .databaseBuilder(application, GithubDB::class.java, "github")
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDao(githubDB: GithubDB): GithubDAO {
+        return githubDB.gethubDAO()
+    }
+
+}
