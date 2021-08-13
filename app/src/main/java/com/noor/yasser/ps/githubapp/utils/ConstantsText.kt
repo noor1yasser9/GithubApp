916ab23737d006ddffd93e723b2e405cdffcccf2
Noor  Yasser <mrh61037@gmail.com>
2021-08-13 18:21:38 +0300

    last update

--- app/src/main/java/com/noor/yasser/ps/githubapp/utils/ConstantsText.kt
+++ app/src/main/java/com/noor/yasser/ps/githubapp/utils/ConstantsText.kt
@@ -7,7 +7,7 @@ const val BASE_URL = "https://api.github.com"
 const val KET_AUTH = "Authorization"
 const val TYPE_VALUE = "application/json"
 const val CONTENT_TYPE = "content-type"
-const val VALUE_AUTH = "mau API KEY? Tonton video tutorialnya ya^^"
+const val VALUE_AUTH = "noor1yasser9"
 
 val DEBUG = Boolean.parseBoolean("true")
 
